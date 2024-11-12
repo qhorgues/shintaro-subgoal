92dade1180562d57040e914c3fab380a5c2bb8fd
Quentin Horgues <quentin.horgues@outlook.fr>
2024-11-12 22:28:25 +0100

    test_fire stick

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,33 @@
+import React, { useEffect } from 'react';
+import { FireTVKeyEvent } from 'react-native-fire-tv-keyevent';
 
-export default function Home() {
+const Home = () => {
+    useEffect(() => {
+        const handleKeyEvent = (event) => {
+            if (event.keyCode === FireTVKeyEvent.KEYCODE_DPAD_CENTER) {
+                alert('Bonjour');
+            }
+        };
+
+        FireTVKeyEvent.addListener(handleKeyEvent);
+
+        return () => {
+            FireTVKeyEvent.removeListener(handleKeyEvent);
+        };
+    }, []);
+
+  return (
+    <div>
+      <h1>Appuyez sur "Entrée" pour afficher un message</h1>
+    </div>
+  );
+};
+
+export default Home;
+
+
+/* export default function Home() {
     return (
         <img src="donation-goal.png" width={"100%"} alt="La liste des sub goal"></img>
     );
-}
+} */
